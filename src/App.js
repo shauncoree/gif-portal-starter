21f9cbda86f6fb32b829bce6b76e6813fa23a2c1
shauncoree <shauncoree@gmail.com>
2022-10-13 23:15:11 +0100

    Wallet and Gif Grid Integration

--- src/App.js
+++ src/App.js
@@ -1,19 +1,125 @@
-import twitterLogo from './assets/twitter-logo.svg';
-import './App.css';
+import twitterLogo from "./assets/twitter-logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const TEST_GIFS = [
+    "https://media.giphy.com/media/xR1YmDbkOhXEKkH3lQ/giphy-downsized-large.gif",
+    "https://media.giphy.com/media/j2m09WzXhnZatCZ4MT/giphy-downsized-large.gif",
+    "https://media.giphy.com/media/VEZiqFSVNnXkorPFu9/giphy-downsized-large.gif",
+    "https://media.giphy.com/media/VIjh7WTkSeb02IX8cI/giphy.gif",
+  ];
+
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [gifList, setGifList] = useState([]);
+
+  const checkIfWalletIsConnect = async () => {
+    if (window?.solana?.isPhantom) {
+      console.log("Phantom Wallet Found!");
+
+      const response = await window.solana.connect({ onlyIfTrusted: true });
+      console.log("Connected with Public Key", response.publicKey.toString());
+
+      setWalletAddress(response.publicKey.toString());
+    } else {
+      alert("Solana Object not found! Get a Phantom wallet! BOO!👻");
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log("Gif Link:", inputValue);
+      setGifList([...gifList, inputValue]);
+      setInputValue('');
+    } else {
+      console.log("Empty input. Try Again.");
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendGif();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter gif link!"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button type="submit" className="cta-button submit-gif-button">
+          Submit
+        </button>
+      </form>
+
+      <div className="gif-grid">
+        {gifList.map((gif) => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnect();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  useEffect(() => {
+    if (walletAddress) {
+      console.log("Fetching GIF list....");
+      // call Sol project
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
+
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? "authed-container" : "container"}>
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">Real-World Metaverse</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
+          {walletAddress && renderConnectedContainer()}
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -22,7 +128,9 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >
+            {`built on @${TWITTER_HANDLE}`} <br></br>
+          </a>
         </div>
       </div>
     </div>
